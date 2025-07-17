abb129627281743ffa7136090c093ad70be32af4
JFu-Z <yao15260@sina.com>
2025-07-17 11:02:14 +0800

    main.py

--- main.py
+++ main.py
@@ -311,7 +311,7 @@ class LambdaLR:
 parser = argparse.ArgumentParser()
 parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
-parser.add_argument("--dataset_name", type=str, default="dataset/datas",
+parser.add_argument("--dataset_name", type=str, default="dataset/train",
                     help="name of the dataset")  ## ../input/facades-dataset
 parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0003, help="adam: learning rate")
@@ -552,82 +552,8 @@ def train():
             torch.save(G_BA.state_dict(), "save/%s/G_BA_%d.pth" % (opt.dataset_name, epoch))
             torch.save(D_A.state_dict(), "save/%s/D_A_%d.pth" % (opt.dataset_name, epoch))
             torch.save(D_B.state_dict(), "save/%s/D_B_%d.pth" % (opt.dataset_name, epoch))
-    open('g_MPCSA','w',encoding='utf-8').write(json.dumps(g_loss))
-    open('d_MPCSA','w',encoding='utf-8').write(json.dumps(d_loss))
 
-def test():
-    ## 超参数设置
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
-    parser.add_argument('--dataroot', type=str, default='dataset/test', help='root directory of the dataset')
-    parser.add_argument('--channels', type=int, default=3, help='number of channels of input data')
-    parser.add_argument('--n_residual_blocks', type=int, default=9, help='number of channels of output data')
-    parser.add_argument('--height', type=int, default=384, help='size of the data (squared assumed)')
-    parser.add_argument('--width', type=int, default=512, help='size of the data (squared assumed)')
-    parser.add_argument('--cuda', type=bool, default=True, help='use GPU computation')
-    parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-    parser.add_argument('--generator_A2B', type=str, default='save/dataset/main/G_AB_198.pth',
-                        help='A2B generator checkpoint file')
-    parser.add_argument('--generator_B2A', type=str, default='save/dataset/main/G_BA_198.pth',
-                        help='B2A generator checkpoint file')
-    opt = parser.parse_args()
-    print(opt)
-
-    #################################
-    ##          test准备工作        ##
-    ## input_shape
-    start_time = time.time()
-    input_shape = (opt.channels, opt.height, opt.width)
-    ## 创建生成器，判别器对象
-    netG_A2B = GeneratorResNet(input_shape, opt.n_residual_blocks)
-    netG_B2A = GeneratorResNet(input_shape, opt.n_residual_blocks)
-    ## 使用cuda
-    if opt.cuda:
-        netG_A2B.cuda()
-        netG_B2A.cuda()
-    ## 载入训练模型参数
-    netG_A2B.load_state_dict(torch.load(opt.generator_A2B))
-    netG_B2A.load_state_dict(torch.load(opt.generator_B2A))
-    ## 设置为测试模式
-    netG_A2B.eval()
-    netG_B2A.eval()
-    ## 创建一个tensor数组
-    Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
-    input_A = Tensor(opt.batchSize, opt.channels, opt.height, opt.width)
-    input_B = Tensor(opt.batchSize, opt.channels, opt.height, opt.width)
-    '''构建测试数据集'''
-    transforms_ = [transforms.ToTensor(),
-                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-    dataloader = DataLoader(ImageDataset(opt.dataroot, transforms_=transforms_, mode='test'),
-                            batch_size=opt.batchSize, shuffle=False, num_workers=opt.n_cpu)
-    #################################
-    ##           test开始          ##
-    #################################
-    '''如果文件路径不存在, 则创建一个 (存放测试输出的图片)'''
-    if not os.path.exists('output/A'):
-        os.makedirs('output/A')
-    if not os.path.exists('output/B'):
-        os.makedirs('output/B')
-
-    for i, batch in enumerate(dataloader):
-        ## 输入数据 real
-        real_A = Variable(input_A.copy_(batch['A']))
-        real_B = Variable(input_B.copy_(batch['B']))
-        ## 通过生成器生成的 fake
-        fake_B = 0.5 * (netG_A2B(real_A).data + 1.0)
-        fake_A = 0.5 * (netG_B2A(real_B).data + 1.0)
-        ## 保存图片
-        save_image(fake_A, 'output/A/%02dA.png' % (i + 1))
-        save_image(fake_B, 'output/B/%02dB.png' % (i + 1))
-        print('processing (%02d)-th image...' % (i))
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"测试耗时:{execution_time}秒")
-    dummy_input = torch.randn(1, 3, 256, 256).to('cuda')
-    flops, params = profile(netG_A2B, (dummy_input,))
-    print('flops: %.4fG, params: %.4fM' % (flops / 1000000000.0, params / 1000000.0))
 
 ## 函数的起始
 if __name__ == '__main__':
-    train()  ## 训练
-    #test()
+    train()  
